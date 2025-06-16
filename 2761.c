93fd84566080737675c74c97ff49c2e36eed2208
Bernardo Soledade <be.soledade@outlook.com>
2025-06-16 10:20:53 -0300

    Add files via upload

---
+++ 2761.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int A;
+    float B;
+    char C;
+    char D[51];
+
+    scanf("%d %f %c %[^\n]", &A, &B, &C, D);
+
+    printf("%d%.6f%c%s\n", A, B, C, D);
+    printf("%d\t%.6f\t%c\t%s\n", A, B, C, D);
+    printf("%10d%10.6f%10c%10s\n", A, B, C, D);
+
+    return 0;
+}
